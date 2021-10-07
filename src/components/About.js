248d25de8e5395cc562a7348a8265ee8be4707e4
sleepeels <lior.webdev@gmail.com>
2021-10-07 17:35:14 +0300

    fix content spelling

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ const About = () => {
     <section id="about">
       <p>Hi, my name is Lior, all around web developer.</p>
       <p>
-        My formal education includes a BSc Computers Engineering and a Teaching
-        Certificate for High School Physics Education. Over the last 4-5 years I
-        have gained a massive experience in Web Development. HTML, CSS &
-        Javascript on the front-end, NodeJS, MongoDB & NPM libraries on the
-        Back-end. I learned and experienced layout design, colors, animations
-        etc.
+        My formal education includes a BSc in Computers Engineering and a
+        Teaching Certificate for High School Physics Education. Over the last
+        4-5 years I have gained a massive "hands-on" experience in Web
+        Development. HTML, CSS & Javascript on the front-end, NodeJS, MongoDB &
+        NPM libraries on the Back-end. I learned and experienced layout design,
+        colors, animations etc.
       </p>
       <p></p>
       <p></p>
