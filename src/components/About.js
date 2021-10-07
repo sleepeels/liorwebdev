79234cc2b0485f5a1f2fdd28a9e8468176d410ba
sleepeels <lior.webdev@gmail.com>
2021-10-08 00:06:05 +0300

    content

--- src/components/About.js
+++ src/components/About.js
@@ -3,18 +3,25 @@ import "./About.css";
 const About = () => {
   return (
     <section id="about">
-      <p>Hi, my name is Lior, all around web developer.</p>
+      <p>Hi, my name is Lior. I can Design, Develop & Deploy.</p>
       <p>
-        My formal education includes a BSc in Computers Engineering and a
-        Teaching Certificate for High School Physics Education. Over the last
-        4-5 years I have gained a massive "hands-on" experience in Web
-        Development. HTML, CSS & Javascript on the front-end, NodeJS, MongoDB &
-        NPM libraries on the Back-end, Apps Deployments to Cloud Services such
-        as AWS, Heroku and Firebase. I learned and experienced layout design,
-        colors, animations etc.
+        Over the last 4-5 years I have gained a massive "Hands-on" Experience in
+        Web Development. HTML, CSS & Javascript on the Front-end, NodeJS,
+        MongoDB & NPM libraries on the Back-end, Apps Deployments to Cloud
+        Services such as AWS, Heroku and Firebase.
       </p>
-      <p></p>
-      <p></p>
+      <p>
+        I Learned and Experienced Layout Design, Colors, CSS Web Animations
+        combined with Figma and some other Editing Softwares Assets.
+      </p>
+      <p>
+        My Formal Education includes a BSc in Computers Engineering and a
+        Teaching Certificate for High School Physics Education.
+      </p>
+      <p>
+        I Teached 11th graders Client - Server Programming using Java Backend as
+        well as basic HTML and javascript for 9th graders.
+      </p>
     </section>
   );
 };
