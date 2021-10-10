8a7e240a4d3f511c19083ca2e83c88fcf678a1f3
Lior <lior.webdev@gmail.com>
2021-10-10 18:13:48 +0300

    add UC

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import StarsReview from "./partials/StarsReview";
 import UnderConst from "./partials/UnderConst";
-
+import "./Contact.css";
 const Contact = () => {
   return (
     <section id="contact">
